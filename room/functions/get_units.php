add5c1352b2ae1b7e833ba2eee79fa6421aa4c74
percy <percymichael0@gmail.com>
2022-05-24 12:13:17 -0700

    Added the abiltiy to add tenant and making the room occupied by turning on a checkbox --- next am gonna add the ability to make a house vacant by turning off the checkbox

--- room/functions/get_units.php
+++ room/functions/get_units.php
@@ -1,12 +1,29 @@
 <?php
 require("../../db_config.php");
 
-$sql = "SELECT rooms.id as id,unit_name,monthly_rent,tenants.names as tenant_names,tenants.contact as tenant_contact FROM rooms LEFT JOIN tenants ON rooms.tenant_id=tenants.id";
+$sql = "SELECT rooms.vacancy as room_status,rooms.id as id,unit_name,monthly_rent,tenants.names as tenant_names,tenants.contact as tenant_contact FROM rooms LEFT JOIN tenants ON rooms.tenant_id=tenants.id";
 $query = mysqli_query($conn, $sql);
 $number = 0;
 if (mysqli_num_rows($query) > 0) {
     while ($row = mysqli_fetch_assoc($query)) {
         $number += 1;
+        if ($row['room_status']==1) {
+            $room_status='checked';
+            $description='Occupied';
+        }
+        else{
+            $room_status='';
+            $description='Empty';
+        }
+
+        if (!empty($row['tenant_contact'])) {
+            $tenant_contact='<i class="fas fa-phone    "></i>'.$row['tenant_contact'];
+        }
+        else{
+            $tenant_contact='This house is vacant';
+            $room_status='';
+
+        }
         echo '
         <tr class="">
             <th scope="row">' . $number . '</th>
@@ -16,13 +33,13 @@ if (mysqli_num_rows($query) > 0) {
                 </td>
                 <td>
                     <div class="form-check form-switch">
-                        <input value="'.$row['id'].'" data-name="'.$row['unit_name'].'" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                        <label class="form-check-label text-muted " for="flexSwitchCheckDefault"><small><i class="status">Empty</i></label>
+                        <input '.$room_status.' value="'.$row['id'].'" data-name="'.$row['unit_name'].'" class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                        <label class="form-check-label text-muted " for="flexSwitchCheckDefault"><small><i class="status">'.$description.'</i></label>
                     </div>
                 </td>
                 <td>
                 <span class="fw-bold">'.$row['tenant_names'].'</span><br /><small class="text-muted text-success">
-                <i class="fas fa-phone    "></i> '.$row['tenant_contact'].'</small>
+                '.$tenant_contact.'</small>
                 </td>
                 <td><span class="fw-bold text-danger">UGX 300,000</span></td>
                         <td></td>
