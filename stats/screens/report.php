ccd3703a928038ae81308d25c9959d86481c670d
DIZAYN <dizayn@DIZAYNs-iMac.local>
2022-08-02 15:43:52 +0300

    Added progress for occupants

--- stats/screens/report.php
+++ stats/screens/report.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+
 if (empty($_SESSION['id'])) {
     header('location:/real/index.php');
     }
@@ -9,12 +10,22 @@ if (empty($_SESSION['id'])) {
     require('../../ui/obj_header.php');
     
     //get company name
+    require("../../db_config.php");
+    $get_filled="SELECT COUNT(id) as num FROM rooms WHERE vacancy=1 and user_id=".$_SESSION['id'];
+    $get_filled_query=mysqli_query($conn,$get_filled);
     
 ?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
+<script
+src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js">
+</script>
     <title>Stats</title>
     
 </head>
@@ -40,10 +51,27 @@ if (empty($_SESSION['id'])) {
         
        
         <div class="flex-fill m-2 card mb-3 border-0" style="box-shadow: rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px;">
-  <div class="card-header">Header</div>
+  <div class="card-header">Occupancy</div>
   <div class="card-body">
-    <h5 class="card-title">Primary card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <div class="d-flex flex-row justify-content-between">
+      <div>
+        <h5 class="card-title"><span class="badge rounded-pill bg-success"> 1 of 10</span></h5>
+        <small class="text-muted">Occupied</small>
+      </div>
+
+      <div>
+
+        <h5 class="card-title"><span class="badge rounded-pill bg-danger">1 of 10</span></h5>
+        <small class="text-muted">Empty</small>
+      </div>
+      
+    </div>
+    
+    <p class="card-text">
+    
+    <canvas id="myChart" style="width:100%;max-width:600px;margin:30px auto"></canvas>
+
+    </p>
   </div>
 </div>
         
@@ -65,7 +93,60 @@ if (empty($_SESSION['id'])) {
     </div>
         <div name="magin bottom" style="height:20px;"></div>
 
+        <script>
+          var data = {
+  labels: [
+    "Occupied",
+    "Empty",
+    
+  ],
+  datasets: [
+    {
+      data: [300, 50],
+      backgroundColor: [
+        "#FF6384",
+        "#36A2EB",
+        
+      ],
+      hoverBackgroundColor: [
+        "#FF6384",
+        "#36A2EB",
+        
+      ]
+    }]
+};
 
+Chart.pluginService.register({
+  beforeDraw: function(chart) {
+    var width = chart.chart.width,
+        height = chart.chart.height,
+        ctx = chart.chart.ctx;
+
+    ctx.restore();
+    var fontSize = (height / 114).toFixed(2);
+    ctx.font = fontSize + "em sans-serif";
+    ctx.textBaseline = "middle";
+
+    var text = "70%",
+        textX = Math.round((width - ctx.measureText(text).width) / 2),
+        textY = height / 2;
+
+    ctx.fillText(text, textX, textY);
+    ctx.save();
+  }
+});
+
+var chart = new Chart(document.getElementById('myChart'), {
+  type: 'doughnut',
+  data: data,
+  options: {
+  	responsive: true,
+    legend: {
+      display: false
+    }
+  }
+});
+        </script>
 
     
 
