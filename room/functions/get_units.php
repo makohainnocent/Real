c4833dd1e1707cd015c6efb8d4060b04eb44770f
DIZAYN <dizayn@DIZAYNs-iMac.local>
2022-08-12 15:03:47 +0300

    remove coulumn to display receipt description and date, which finally fixed the issue of duplicate house view

--- room/functions/get_units.php
+++ room/functions/get_units.php
@@ -7,7 +7,7 @@ require("../../db_config.php");
 $estate_id=$_POST['id'];
 
 
-$sql = "SELECT DISTINCT(receipts.description) as rd,last_notified_date as lnd,last_receipt_date as lrd,DATEDIFF(CURDATE(),last_receipt_date) as days,rooms.balance as balance,rooms.estate_id as estate_id,rooms.vacancy as room_status,rooms.id as id,unit_name,monthly_rent,tenants.names as tenant_names,tenants.contact as tenant_contact FROM rooms LEFT JOIN tenants ON rooms.tenant_id=tenants.id LEFT JOIN receipts ON rooms.last_receipt_date=receipts.date WHERE rooms.estate_id='$estate_id' ORDER BY rooms.vacancy,rooms.id";
+$sql = "SELECT DISTINCT last_notified_date as lnd,DATEDIFF(CURDATE(),last_receipt_date) as days,rooms.balance as balance,rooms.estate_id as estate_id,rooms.vacancy as room_status,rooms.id as id,unit_name,monthly_rent,tenants.names as tenant_names,tenants.contact as tenant_contact FROM rooms LEFT JOIN tenants ON rooms.tenant_id=tenants.id WHERE rooms.estate_id='$estate_id' ORDER BY rooms.vacancy,rooms.id";
 $query = mysqli_query($conn, $sql) or die('Error');
 $number = 0;
 $current_date=date("Y-m-d");
@@ -80,7 +80,7 @@ if (mysqli_num_rows($query) > 0) {
                 '.$tenant_contact.'</small>
                 </td>
                 <td><span class="fw-bold '.$balance_color.'">UGX '.number_format($row['balance']).'</span>'.$row['days'].'</td>
-                <td><small style="font-size:13px;" class="text-muted"><i>'.$last_receipt_date.'<i/><br>'.$row['rd'].'<small></td>
+                
                         <td><button data-balance="'.$row['balance'].'" data-estate-id="'.$row['estate_id'].'" data-room-id="'.$row['id'].'" '.$disabled.' data-tenant-names="'.$row['tenant_names'].'" id="triger_receipt_popup" type="button" class="btn btn-success btn-sm"><i class="fas fa-receipt"></i> RECEIPT</button> 
                         <button data-room-name="'.$row['unit_name'].'" data-room-id="'.$row['id'].'" id="show_all_receipts_for_this_house" type="button" class="btn btn-outline-warning btn-sm"><i class="fas fa-eye"></i> VIEW</button></td>
         </tr>
--- room/screens/view-houses.php
+++ room/screens/view-houses.php
@@ -12,232 +12,232 @@ if ($get_estate_name_query) {
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <title>Units</title>
-    <?php require('../../ui/obj_header.php') ?>
-    
-    
-</head>
+    <head>
+        <title>Units</title>
+        <?php require('../../ui/obj_header.php') ?>
 
-<body id="body-pd" style="background-color:#f6efe5 !important;">
 
-    <header class="header" id="header" style="background-color: #f6efe5 !important;">
-        <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
-        <span class="h5 " style="font-weight:800;color:#3ab36e;"><?php echo strtoupper($_SESSION['company_name']); ?></span>
-        <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
-    </header>
-    <?php require('../../sidebar.php') ?>
-    <!--Container Main start-->
-    <div class="height-100 bg-light mt-3" style="background-color:#f6efe5 !important;">
-        <h4>Main Components</h4>
+    </head>
 
-        <div class="table-wrapper mt-5 flex">
-            <div class=""></div>
-            <div class=""></div>
-            <div class=""></div>
-            <div class=""></div>
+    <body id="body-pd" style="background-color:#f6efe5 !important;">
+
+        <header class="header" id="header" style="background-color: #f6efe5 !important;">
+            <div class="header_toggle"> <i class='bx bx-menu' id="header-toggle"></i> </div>
+            <span class="h5 "
+                style="font-weight:800;color:#3ab36e;"><?php echo strtoupper($_SESSION['company_name']); ?></span>
+            <div class="header_img"> <img src="https://i.imgur.com/hczKIze.jpg" alt=""> </div>
+        </header>
+        <?php require('../../sidebar.php') ?>
+        <!--Container Main start-->
+        <div class="height-100 bg-light mt-3" style="background-color:#f6efe5 !important;">
+            <h4>Main Components</h4>
+
+            <div class="table-wrapper mt-5 flex">
+                <div class=""></div>
+                <div class=""></div>
+                <div class=""></div>
+                <div class=""></div>
+            </div>
+
+
+            <div class="table-wrapper mt-5"
+                style="background: #fff;padding: 20px;box-shadow: 0 1px 1px rgb(0 0 0 / 5%);">
+
+                <div class="header-container d-flex flex-row justify-content-end">
+
+                    <div>
+                        <button type="button" class="btn btn-outline-success" data-bs-toggle="modal"
+                            data-bs-target="#add_house"><i class="fa fa-plus" aria-hidden="true"></i> New House</button>
+                    </div>
+
+                    <div class="flex-fill">
+                        <h2 style="text-align:center">
+                            <?php echo $name['estate_name']; ?>
+                            Estate
+                        </h2>
+                    </div>
+
+                </div>
+
+
+
+
+
+                <table class="table table-hover mt-3 mx-auto caption-top">
+                    <thead class="thead-light">
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Unit Name</th>
+                            <th scope="col">Status</th>
+                            <th scope="col">Tenant Name</th>
+                            <th scope="col">Balance</th>
+                            <!--<th scope="col">Prev Receipt</th>-->
+                            <th scope="col">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody id="units">
+
+
+                    </tbody>
+                </table>
+            </div>
+            <div name="magin bottom" style="height:20px;"></div>
         </div>
 
 
-        <div class="table-wrapper mt-5" style="background: #fff;padding: 20px;box-shadow: 0 1px 1px rgb(0 0 0 / 5%);">
 
-        <div class="header-container d-flex flex-row justify-content-end">
-
-         <div>
-         <button type="button" class="btn btn-outline-success" data-bs-toggle="modal" data-bs-target="#add_house"><i class="fa fa-plus" aria-hidden="true"></i> New House</button>
-         </div>
-
-        <div class="flex-fill">
-         <h2 style="text-align:center" >
-         <?php echo $name['estate_name']; ?>
-           Estate
-         </h2>
-        </div>
-
-         </div> 
-
-            
-            
-
-
-            <table class="table table-hover mt-3 mx-auto caption-top">
-                <thead class="thead-light">
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">Unit Name</th>
-                        <th scope="col">Status</th>
-                        <th scope="col">Tenant Name</th>
-                        <th scope="col">Balance</th>
-                        <th scope="col">Prev Receipt</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody id="units">
-                    
-
-                </tbody>
-            </table>
-        </div>
-        <div name="magin bottom" style="height:20px;"></div>
-    </div>
-
-
-
-    <?php
+        <?php
     require("../screens/modals.html");
     ?>
-    <?php
+        <?php
     echo "<script>
     let this_estate_id=".$_GET['id']."
     
     </script>";
     ?>
-    <script>
+        <script>
         //buttons logic
 
         //display modal to show all previous receipts
-         $(document).on('click', '#show_all_receipts_for_this_house', function() {
-             var id = $(this).attr('data-room-id');
-             var room_name_for_title = $(this).attr('data-room-name');
-             $('.receipt-tenant-modal-title').html('Receipts for house '+room_name_for_title);
+        $(document).on('click', '#show_all_receipts_for_this_house', function() {
+            var id = $(this).attr('data-room-id');
+            var room_name_for_title = $(this).attr('data-room-name');
+            $('.receipt-tenant-modal-title').html('Receipts for house ' + room_name_for_title);
 
             $.ajax({
                 url: "../functions/get_receipts.php",
                 type: "POST",
                 data: {
-                        room_id:id,
-                    },
+                    room_id: id,
+                },
                 cache: false,
                 success: function(data) {
                     $('#receipts_table').html(data);
                 }
             });
 
-             $('#show_receipts').modal('show');
-         });
+            $('#show_receipts').modal('show');
+        });
 
 
         //first popup the receipt form
-        var room_id='';
-        var estate_id='';
+        var room_id = '';
+        var estate_id = '';
         $(document).on('click', '#triger_receipt_popup', function() {
-            var tenant_names=$(this).attr('data-tenant-names');
-            estate_id=$(this).attr('data-estate-id');
-            room_id=$(this).attr('data-room-id');
-            $('.payment-receipt-modal-title').html('Make Payment Receipt for '+tenant_names);
+            var tenant_names = $(this).attr('data-tenant-names');
+            estate_id = $(this).attr('data-estate-id');
+            room_id = $(this).attr('data-room-id');
+            $('.payment-receipt-modal-title').html('Make Payment Receipt for ' + tenant_names);
             $('#make_payment_receipt').modal('show');
-            
-            
-            
-        }
-        );
-        
+
+
+
+        });
+
         //then insert the receipt on button click
         $(document).on('click', '#insert_payment_receipt', function() {
 
-            var payment=$('#payment_method').val();
-            var amount=$('#receipt_amount').val();
-            var date=$('#receipt_date').val();
-            var description=$('#receipt_description').val();
+            var payment = $('#payment_method').val();
+            var amount = $('#receipt_amount').val();
+            var date = $('#receipt_date').val();
+            var description = $('#receipt_description').val();
 
             $.ajax({
                 url: '../functions/make_receipt.php',
                 type: 'post',
-                    data: {
-                        room_id:room_id,
-                        receipt_amount:amount,
-                        receipt_date:date
-                    },
-                    success: function() {
-                        alert('Receipt was made')
-                        displayUnits();
-                    }
-                });
+                data: {
+                    room_id: room_id,
+                    receipt_amount: amount,
+                    receipt_date: date
+                },
+                success: function() {
+                    alert('Receipt was made')
+                    displayUnits();
+                }
+            });
 
-                 $.ajax({
+            $.ajax({
                 url: '../functions/insert_receipt.php',
                 type: 'post',
-                    data: {
-                        estate_id:estate_id,
-                        room_id:room_id,
-                        receipt_amount:amount,
-                        receipt_date:date,
-                        method:payment,
-                        description:description
-                    },
-                    success: function(res) {
-                        var res1 = JSON.parse(res);
-                        if(res1.statusCode==201){
-                            alert(res1.error+" Hello");
-                        }
-                        else{
+                data: {
+                    estate_id: estate_id,
+                    room_id: room_id,
+                    receipt_amount: amount,
+                    receipt_date: date,
+                    method: payment,
+                    description: description
+                },
+                success: function(res) {
+                    var res1 = JSON.parse(res);
+                    if (res1.statusCode == 201) {
+                        alert(res1.error + " Hello");
+                    } else {
 
-                            alert('Receipt was successfully inserted')
-                            displayUnits();
-                        }
+                        alert('Receipt was successfully inserted')
+                        displayUnits();
                     }
-                });
-            
+                }
+            });
+
         });
 
         var room_id;
         var room_name;
         $(document).on('click', '#add_tenant_to_room', function() {
-            var tenant_id=$('#tenant_id').val();
-            var date=$('#date').val();
-            if (tenant_id=='' || date=='') {
+            var tenant_id = $('#tenant_id').val();
+            var date = $('#date').val();
+            if (tenant_id == '' || date == '') {
                 alert('All fields are required!');
                 return false;
             }
 
-            
-            
+
+
             //alert(room_id+' '+tenant_id);
-            
-            
+
+
             $.ajax({
                 url: '../functions/insert_tenant_to_room.php',
                 type: 'post',
-                    data: {
-                        tenant_id: tenant_id,
-                        id: room_id,
-                        date:date
-                    },
-                    success: function() {
-                        alert('Tenant was added to the house')
-                        displayUnits();
-                    }
-                });
+                data: {
+                    tenant_id: tenant_id,
+                    id: room_id,
+                    date: date
+                },
+                success: function() {
+                    alert('Tenant was added to the house')
+                    displayUnits();
+                }
+            });
         })
-        
-        
+
+
         //Change text of switch and show popup to insert user
         $(document).on('change', '.form-check-input', function() {
-            
-            if(this.checked) {
+
+            if (this.checked) {
                 $(this).siblings('label').html('Occupied');
-                room_name=$(this).attr('data-name');
-                room_id=$(this).attr('value');
-                $('.insert-tenant-modal-title').text("Add Tenant to "+room_name);
+                room_name = $(this).attr('data-name');
+                room_id = $(this).attr('value');
+                $('.insert-tenant-modal-title').text("Add Tenant to " + room_name);
                 $('#insert_tenant').modal('show');
-                
+
                 // Display the returned user names and id data in browser
                 $.get("../functions/select_tenants.php", function(data) {
                     $("#tenant_id").html(data);
                 });
-                
 
-                
-            }
-            else{
+
+
+            } else {
                 //remove tenant from house
-                room_name=$(this).attr('data-name');
-                room_id=$(this).attr('value');
-                if (confirm("Are you sure you want to make "+room_name+" vacant? ")) {
-                    
+                room_name = $(this).attr('data-name');
+                room_id = $(this).attr('value');
+                if (confirm("Are you sure you want to make " + room_name + " vacant? ")) {
+
                     $.ajax({
-                    url: '../functions/remove_tenant.php',
-                    type: 'post',
+                        url: '../functions/remove_tenant.php',
+                        type: 'post',
                         data: {
                             id: room_id
                         },
@@ -247,11 +247,10 @@ if ($get_estate_name_query) {
                         }
                     });
                     $(this).siblings('label').html('Empty');
-                }
-                else{
+                } else {
                     displayUnits();
                 }
-                
+
             }
         });
 
@@ -260,11 +259,11 @@ if ($get_estate_name_query) {
                 url: "../functions/get_units.php",
                 type: "POST",
                 data: {
-                            id:this_estate_id
-                        },
+                    id: this_estate_id
+                },
                 cache: false,
                 success: function(data) {
-                    
+
                     $('#units').html(data);
                 }
             });
@@ -310,7 +309,8 @@ if ($get_estate_name_query) {
                 $('#updateModal').modal('show');
                 $('#estateName').val(estate_name);
                 $('#estateLocation').val(estate_location);
-                var html_code = "<option selected value=" + estate_owner_id + ">" + estate_owner_names + "</option>";
+                var html_code = "<option selected value=" + estate_owner_id + ">" + estate_owner_names +
+                    "</option>";
                 $('#ownerId').html(html_code);
                 $('#estateDetails').val(estate_details);
 
@@ -366,7 +366,7 @@ if ($get_estate_name_query) {
                             data: {
                                 unit_name: unit_name,
                                 rent: rent,
-                                estate_id:this_estate_id
+                                estate_id: this_estate_id
 
                             },
 
@@ -391,12 +391,12 @@ if ($get_estate_name_query) {
                 }
             );
         }
-    </script>
+        </script>
 
 
 
 
 
-</body>
+    </body>
 
 </html>
